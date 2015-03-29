5fb7c0165ea1d4c3016556065bd7eee42e4e7515
AlexandrErokhin <11whitelex11@gmail.com>
2015-03-29 14:51:04 +0400

    Начал интерфейс

---
+++ LFC/RegPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+
+namespace LFC
+{
+    public partial class RegPage : PhoneApplicationPage
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+        }
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+   
+}
