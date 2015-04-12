105428933c0f58048defd12dad7a3ff8c1d56e8f
GennadyChemyakin <gmeinpost@gmail.com>
2015-04-13 00:12:36 +0300

    Добавил обработчик к кнопке регистрации

--- LFC/RegPage.xaml.cs
+++ LFC/RegPage.xaml.cs
@@ -21,6 +21,11 @@ namespace LFC
         public RegPage()
         {
             InitializeComponent();
+            Reg.Click += ((object sender, RoutedEventArgs e) => {
+                WebBrowserTask webBrowserTask = new WebBrowserTask();
+                webBrowserTask.Uri = new Uri("http://lastfm.ru/join", UriKind.Absolute);
+                webBrowserTask.Show();
+            });
 
         }
         private async void Enter_Click(object sender, RoutedEventArgs e)
@@ -41,13 +46,6 @@ namespace LFC
                 authProgress.IsIndeterminate = false;
                 MessageBox.Show(msg);
             }
-        }
-
-        private void Reg_Clicked(object sender, RoutedEventArgs e)
-        {
-            WebBrowserTask webBrowserTask = new WebBrowserTask();
-            webBrowserTask.Uri = new Uri("http://lastfm.ru/join", UriKind.Absolute);
-            webBrowserTask.Show();
         }
     }
 }
