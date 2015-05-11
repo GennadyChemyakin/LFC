619c8167f241722243f082fc624944d5b3685d1e
GennadyChemyakin <gmeinpost@gmail.com>
2015-05-11 04:39:58 +0300

    Добавление/удаление из библиотеки

--- LFC/Library.xaml.cs
+++ LFC/Library.xaml.cs
@@ -26,15 +26,15 @@ namespace LFC
         }
 
         private async void Library_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {           
+        {
             switch (LibraryPanorama.SelectedIndex)
             {
                 case 0: // рекомендации
                     yourRecomPB.IsIndeterminate = true;
                     try
                     {
-                    artists = await client.userGetRecommendedArtists(auth.UserName);
-                    yourRecomList.ItemsSource = artists;
+                        artists = await client.userGetRecommendedArtists(auth.UserName);
+                        yourRecomList.ItemsSource = artists;
                     }
                     catch (Exception err)
                     {
@@ -47,8 +47,8 @@ namespace LFC
                     yourMusicPB.IsIndeterminate = true;
                     try
                     {
-                    tracks = await client.libraryGetTracks(auth.UserName);
-                    yourMusicList.ItemsSource = tracks;
+                        tracks = await client.libraryGetTracks(auth.UserName);
+                        yourMusicList.ItemsSource = tracks;
                     }
                     catch (Exception err)
                     {
@@ -62,8 +62,8 @@ namespace LFC
                     artistPB.IsIndeterminate = true;
                     try
                     {
-                    artists = await client.libraryGetArtists(auth.UserName);
-                    artistList.ItemsSource = artists;
+                        artists = await client.libraryGetArtists(auth.UserName);
+                        artistList.ItemsSource = artists;
                     }
                     catch (Exception err)
                     {
@@ -73,19 +73,19 @@ namespace LFC
 
                     artistPB.IsIndeterminate = false;
                     break;
-                case 3: // недавние
+                case 4: // недавние
                     recentPlayLPB.IsIndeterminate = true;
                     try
                     {
-                    tracks = await client.userGetRecentTracks(auth.UserName);
-                    recentPlayLList.ItemsSource = tracks;
+                        tracks = await client.userGetRecentTracks(auth.UserName);
+                        recentPlayLList.ItemsSource = tracks;
                     }
                     catch (Exception err)
                     {
                         MessageBox.Show("Вы недавно не слушали музыку :(");
                         Console.Write(err.StackTrace);
                     }
-                    
+
                     recentPlayLPB.IsIndeterminate = false;
                     break;
             }
@@ -99,9 +99,8 @@ namespace LFC
                 App.ViewModel.LoadData();
             }
             auth = NavigationService.GetNavigationData().ElementAt(0) as LFCAuth;
-            int item = Convert.ToInt32(NavigationService.GetNavigationData().ElementAt(1));
             client = new Client.Client(auth);
-            LibraryPanorama.SetValue(Panorama.SelectedItemProperty, LibraryPanorama.Items[item]);
+            LibraryPanorama.SetValue(Panorama.SelectedItemProperty, LibraryPanorama.Items[0]);
         }
 
         private async void linkToArtistInfo_Click(object sender, RoutedEventArgs e)
@@ -116,6 +115,55 @@ namespace LFC
             objList.Add(null); // костыль
             objList.Add(ar);
             NavigationService.Navigate(new Uri("/ArtistInfo.xaml", UriKind.Relative), objList);
+        }
+
+        private async void buttonAddToLibrary_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var myobject = button.DataContext;
+            var name = (myobject as LFCArtist).Name;
+            var result = await client.libraryAddArtist(name);
+            if (result == true)
+            {
+                MessageBox.Show("Исполнитель добавлен в вашу библиотеку");
+                yourRecomPB.IsIndeterminate = true;
+                try
+                {
+                    artists = await client.userGetRecommendedArtists(auth.UserName);
+                    yourRecomList.ItemsSource = artists;
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("У вас нет рекомендаций :(");
+                    Console.Write(err.StackTrace);
+                }
+                yourRecomPB.IsIndeterminate = false;
+            }
+        }
+
+        private async void buttonRemoveFromLibrary_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var myobject = button.DataContext;
+            var name = (myobject as LFCArtist).Name;
+            var result = await client.libraryRemoveArtist(name);
+            if (result == true)
+            {
+                MessageBox.Show("Исполнитель удален из вашей библиотеки");
+                artistPB.IsIndeterminate = true;
+                try
+                {
+                    artists = await client.libraryGetArtists(auth.UserName);
+                    artistList.ItemsSource = artists;
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("У вас нет списка исполнителей :(");
+                    Console.Write(err.StackTrace);
+                }
+
+                artistPB.IsIndeterminate = false;
+            }
         }
     }
 }
